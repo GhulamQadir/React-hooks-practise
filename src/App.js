6bc56f9a3aa8d4a5541aa6bcf0eb333ee46a6853
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-30 12:34:26 -0700

    1st hook (useState) practise with examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [name, setName] = useState();
+  const [bool, setBool] = useState(false)
+  const [previousVal, setPreviousVal] = useState()
+  const [count, setCount] = useState(0)
+
+
+
+
+  // Using useState with array
+  const [progLanguages, setProgLanguages] = useState()
+
+
+
+
+
+  function changeName() {
+    setBool(!bool)
+    setName("Ghulam Qadir Sakaria")
+  }
+
+
+
+
+  /////////                Counter App Functionality           ////////
+  function increment() {
+    setCount(count + 1)
+  }
+
+  function decrement() {
+    setPreviousVal(count)
+    setCount(count - 1)
+  }
+
+  function reset() {
+    setPreviousVal(count)
+    setCount(0)
+  }
+
+  function undo() {
+    setCount(previousVal)
+  }
+
+
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <p>useState example 1</p>
+      <h1>My name is {bool ? name : ""}</h1>
+      <button onClick={changeName}>Change Name</button>
+      <hr />
+      <br />
+      <br />
+
+
+      {/*                         Counter App with (useState)                    */}
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button disabled={count === 0} onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={undo}>Undo</button>
+      <hr />
+      <br />
+      <br />
+
+
+
+
+
+    </div>
+  )
+}
 export default App;
